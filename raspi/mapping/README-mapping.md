c9edd17df317f8e94442c48c3f47840774de2ac1
andrew <adority121@gmail.com>
2024-01-15 20:51:07 -0600

    command line execution and plays tones

--- raspi/mapping/README-mapping.md
+++ raspi/mapping/README-mapping.md
@@ -8,11 +8,12 @@ python3 Mapping.py
 # midi notes
 midi note_on message format:
 1001nnnn 0kkkkkkk 0vvvvvvv
-where 1001 = note on message, k = which key to press (21 - 108 are the 88 keys on piano), and v = velocity (how hard to press each key)
+where 1001 = "note on" message, n = which channel to play note on, k = which key to press (21 - 108 are the 88 keys on piano), and v = velocity (how hard to press each key)
 The mido package calculates the time between each key press for us.
 
-fastest pressing time (based on fastest time in Rush E): 0.0011558666666666667 seconds
+fastest pressing time in Rush E: 0.0011546666666666665 seconds
+fastest pressing time in Comptine_Dun_Autre_t_-_Yann_Tiersen: 0.0012499979166666665 seconds
 
-## TODO:
-- Remove unecessary messages from mido's object
-- re-format each message to a new type that only includes necessary information to play song (i.e., channel, key, velocity, time)
+# MuseScore midi simplifications (assertions) (pls test these)
+note_on with 0 velocity is used to unpress every key sometimes
+when note_off is used instead, it seems to have velocity of 0 anyways
--- raspi/mapping/mapping.py
+++ raspi/mapping/mapping.py
@@ -1,18 +1,81 @@
+
 import mido
 
-if __name__ == "__main__":
-    # vars for midi file names
-    comptine = "example_songs/Comptine_Dun_Autre_t_-_Yann_Tiersen.mid"
-    rush_e = "example_songs/rush_e_real.mid"
+# define type aliases
+key_event = dict[str, int]
+score = list[key_event]
 
-    # write note details to file
-    file = open("midi-example.txt", "w")
-    midi_file = mido.MidiFile(rush_e) # change filename here
+"""
+    file: string that represents the relative file location of the midi file
+    returns a score that represents the data to be transmitted to the arduino
+"""
+def Map(file) -> score:
+    raw_data = mido.MidiFile(file)
+    mapping: score = []
+    delta_time: int = 0
+
+    # for every midi message
+    for msg in raw_data:
+        msg_dict = msg.dict()
+        msg_type = msg_dict["type"]
+
+        # if not a note event message, increment delta_time
+        if msg_type != "note_on" and msg_type != "note_off":
+            delta_time += msg_dict["time"]
+            continue
+
+        msg_chan = msg_dict["channel"]
+        msg_note = msg_dict["note"]
+        msg_vel = msg_dict["velocity"]
+        msg_time = msg_dict["time"] + delta_time
+
+        new_key_event: key_event = {
+            #"type": msg_type,
+            #"channel": msg_chan,
+            "note": msg_note,
+            "velocity": msg_vel,
+            "time": msg_time
+        }
+
+        delta_time = 0
+        mapping.append(new_key_event)
+
+    return mapping
+
+
+# TESTING -------------------------------------------------------------------------------------------------
+
+import sys
+import pysine
+import math
+import time
+
+def play_song(mapping: score):
+    for event in mapping:
+        if event["velocity"] > 0:
+            pysine.sine(2**((event["note"]-21)/12)*27.5, event["time"])
+        else:
+            time.sleep(event["time"])
+
+if __name__ == "__main__":
+    
+
+    # interpret command
+    if len(sys.argv) != 2:
+        print(f"Command format: python3 mapping.py [path-to-file]")
+
+    # write note details to file and print fastest pressing time
+    file = open(f"{sys.argv[1]}-output.txt", "w")
+    mapping = Map(sys.argv[1])
     times = []
-    for msg in midi_file:
+    for msg in mapping:
+        if msg["time"] != 0:
+            times.append(msg["time"])
         file.write(str(msg) + "\n")
-        if type(msg) == mido.messages.messages.Message and msg.dict()["time"] != 0: # put time in array if not 0
-            times.append(msg.dict()["time"])
     file.close()
-    # calculate min time
-    print(f"fastest pressing time: {min(times)}")
+
+    print(f"Fastest pressing time: {min(times)}")
+
+    print("Playing song...")
+    play_song(mapping)
+

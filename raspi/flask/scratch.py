3bbf899cb158a7a7f4fa4636597fc3cf147d51e3
William Powers <williampowers@ku.edu>
2024-01-19 10:54:55 -0600

    patching config files

--- raspi/flask/scratch.py
+++ raspi/flask/scratch.py
@@ -1,34 +1,10 @@
-import os
+import configparser
 
-songs_dict = {3: 'Gimme_Gimme_Gimme_A_Man_After_Midnight.mscz.mxl'}
+config = configparser.ConfigParser()
 
+config.read('/home/will/Desktop/config.ini')
 
-mypath = "/home/will/Downloads"
-
-files = os.listdir(mypath)
-
-for file_name in files:
-    if len(songs_dict) == 0:
-        songs_dict[0] = file_name
-    elif file_name in songs_dict.values():
-        pass
-    else:
-        songs_dict[list(songs_dict.keys())[-1]+1] = file_name
+username = config['credentials']['username']
+password = config['credentials']['password']
 
 
-print(songs_dict)
-print("\n\n")
-
-
-def filter_dictionary(original_dict, search_string):
-    result_dict = {}
-
-    for key, value in original_dict.items():
-        if search_string.lower() in value.lower():
-            result_dict[key] = value
-    
-    return result_dict
-
-result = filter_dictionary(songs_dict, "hotline")
-
-print(result)

b519b583a82454df24fe12c2a4ee6f9134113368
andorryu <adority121@gmail.com>
2024-01-20 02:17:26 -0600

    cleaned up code and readme and added more songs to example_songs folder

--- raspi/mapping/README-mapping.md
+++ raspi/mapping/README-mapping.md
@@ -1,23 +1,21 @@
-# install dependencies to run Mapping.py
+
+# DEPENDENCIES
+## dependencies to run Map() function
 pip install mido
-# dependecies for running unit test
+## dependecies for running unit test
 sudo apt install fluidsynth
 pip install scamp
 
-
 # Running Mapping.py
-## (this will only run the code in 'if __name__ == "__main__": block')
 python3 Mapping.py
 
-# midi notes
+# Midi Stuff
 midi note_on message format:
-1001nnnn 0kkkkkkk 0vvvvvvv
-where 1001 = "note on" message, n = which channel to play note on, k = which key to press (21 - 108 are the 88 keys on piano), and v = velocity (how hard to press each key)
-The mido package calculates the time between each key press for us.
+1001cccc 0nnnnnnn 0vvvvvvv
+where 1001 = "note on" message, c = which channel to play note on, n = which note to play, and v = velocity (how hard to press each key)
+- The mido package calculates the time between each key press for us (normally its embedded in the meta messages i think)
+- note values 21 - 108 map to the 88 keys on a piano
+- a message with velocity 0 means to unpress the key
 
-fastest pressing time in Rush E: 0.0011546666666666665 seconds
-fastest pressing time in Comptine_Dun_Autre_t_-_Yann_Tiersen: 0.0012499979166666665 seconds
-
-# MuseScore midi simplifications (assertions) (pls test these)
-note_on with 0 velocity is used to unpress every key sometimes
-when note_off is used instead, it seems to have velocity of 0 anyways
+# TODO
+- Better error checking & documentation
--- raspi/mapping/mapping.py
+++ raspi/mapping/mapping.py
@@ -2,14 +2,13 @@
 import mido
 
 class NoteEvent:
-    def __init__(self, channel, note, velocity, time) -> None:
+    def __init__(self, note, velocity, time) -> None:
         self.note: int = note
         self.velocity: int = velocity
         self.time: float = time
-        self.channel: int = channel
     
     def __str__(self):
-        return f"channel = {self.channel}, note = {' '*(3-len(str(self.note)))}{self.note}, velocity = {' '*(3-len(str(self.velocity)))}{self.velocity}, time = {self.time}"
+        return f"note = {' '*(3-len(str(self.note)))}{self.note}, velocity = {' '*(3-len(str(self.velocity)))}{self.velocity}, time = {self.time}"
 
 timeline = list[NoteEvent]
 
@@ -32,12 +31,11 @@ def Map(file) -> timeline:
             delta_time += msg_dict["time"]
             continue
 
-        msg_chan = msg_dict["channel"]
         msg_note = msg_dict["note"]
         msg_vel = msg_dict["velocity"]
         msg_time = msg_dict["time"] + delta_time
 
-        new_note_event: NoteEvent = NoteEvent(msg_chan, msg_note, msg_vel, msg_time)
+        new_note_event: NoteEvent = NoteEvent(msg_note, msg_vel, msg_time)
 
         delta_time = 0
         mapping.append(new_note_event)
@@ -54,7 +52,7 @@ import signal
 from scamp import *
 
 def play_song(mapping: timeline):
-    s = Session()
+    s = Session(max_threads=10000) # high number of threads so that rush e can play lmao
     piano = s.new_part("piano")
     proc_dict: dict[int, Clock] = {}
     for event in mapping:
@@ -75,17 +73,15 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print(f"Command format: python3 mapping.py [path-to-file]")
 
-    # write note details to file and print fastest pressing time
-    file = open(f"{sys.argv[1]}-output.txt", "w")
+    # do mapping
     mapping = Map(sys.argv[1])
-    times = []
+
+    # write note details to file
+    file = open(f"{sys.argv[1]}-output.txt", "w")
     for msg in mapping:
-        if msg.time != 0:
-            times.append(msg.time)
         file.write(str(msg) + "\n")
     file.close()
 
-    print(f"Fastest pressing time: {min(times)}")
-
+    # play song thru unit test function
     print("Playing song...")
     play_song(mapping)

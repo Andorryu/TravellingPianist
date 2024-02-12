c1fbd663a4f81cd7ab0f60984e516a23b1d379b7
andrew <adority121@gmail.com>
2024-02-12 00:09:33 -0600

    now, flask imports Map function instead of just redefining the function

--- raspi/flask/src/endpoints/upload.py
+++ raspi/flask/src/endpoints/upload.py
@@ -1,7 +1,9 @@
 from flask_restful import Resource, reqparse
 import os
-import mido
 import json
+import sys
+sys.path.insert(0, '../../')
+from mapping.mapping import Map
 
 
 downloads_path = '/home/will/Downloads/'
@@ -17,46 +19,6 @@ def find_song(song_name):
 
 upload_put_args = reqparse.RequestParser()
 upload_put_args.add_argument("name", type=str, help="Not a string song name...")
-
-
-"""
-    file: string that represents the relative file location of the midi file
-    returns a score that represents the data to be transmitted to the arduino
-"""
-
-
-timeline = list[dict]
-
-def Map(file) -> timeline:
-    raw_data = mido.MidiFile(file)
-    mapping: timeline = []
-    delta_time: int = 0
-
-    # for every midi message
-    for msg in raw_data:
-        msg_dict = msg.dict()
-        msg_type = msg_dict["type"]
-
-        # if not a note event message, increment delta_time
-        if msg_type != "note_on" and msg_type != "note_off":
-            delta_time += msg_dict["time"]
-            continue
-
-        msg_note = msg_dict["note"]
-        msg_vel = msg_dict["velocity"]
-        msg_time = msg_dict["time"] + delta_time
-
-        new_note_event: dict = {
-            "note": msg_note,
-            "velocity": msg_vel,
-            "time": msg_time
-        }
-
-        delta_time = 0
-        mapping.append(new_note_event)
-
-    return mapping
-
 
 # endpoint 3 -> upload user selected file to device
 class Upload(Resource):

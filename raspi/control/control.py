8ae50e2de91ce7b39a549ba165b992a3f0c86a94
piano <williampowers@ku.edu>
2024-02-12 14:57:35 -0600

    need to implement offset

--- raspi/control/control.py
+++ raspi/control/control.py
@@ -2,17 +2,59 @@
 import RPi.GPIO as GPIO
 from time import sleep
 import math
+import sys
 import pcf8574_io as pcf
+
+MAX_PI_PINS = 28
+PI_PIN_MAP = [
+    27, # GPIO 0 (ID_SD)
+    28, # GPIO 1 (ID_SC)
+    3, # GPIO 2 (SDA)
+    5, # GPIO 3 (SCL)
+    7, # GPIO 4 (GPCLK0)
+    29, # GPIO 5
+    31, # GPIO 6
+    26, # GPIO 7 (CE1)
+    24, # GPIO 8 (CE0)
+    21, # GPIO 9 (MISO)
+    19, # GPIO 10 (MOSI)
+    23, # GPIO 11 (SCLK)
+    32, # GPIO 12 (PWM0)
+    33, # GPIO 13 (PWM1)
+    8, # GPIO 14 (TXD)
+    10, # GPIO 15 (RXD)
+    36, # GPIO 16
+    11, # GPIO 17
+    12, # GPIO 18 (PCM_CLK)
+    35, # GPIO 19 (PCM_FS)
+    38, # GPIO 20 (PCM_DIN)
+    40, # GPIO 21 (PCM_DOUT)
+    15, # GPIO 22
+    16, # GPIO 23
+    18, # GPIO 24
+    22, # GPIO 25
+    37, # GPIO 26
+    13 # GPIO 27
+]
 
 def chip_test():
     pass
 
-def init_pins() -> list[pcf.PCF]:
-    num_pins = 12 # aka number of keys on piano that we are using
-    offset = 0 # when not using all 88 keys, how far up the piano board should we play?
-    num_chips =  math.ceil(num_pins/8)
-    # create PCF objects - this can handle up to 64 pins
-    chips = [pcf.PCF(i) for i in range(0x20, 0x20+num_chips)]
+# initialize pins on pcf boards
+def init_chip_pins(num_pins, offset) -> list[pcf.PCF]:
+    num_chips =  math.ceil(num_pins/8) # number of pcf boards to use
+
+    chips = [pcf.PCF(i) for i in range(0x20, 0x20+(8 if num_chips > 8 else num_chips))]
+    for chip in chips:
+        chip.set_i2cBus(1)
+
+    # remainder chips if they exist
+    if num_chips > 8:
+        temp = [pcf.PCF(i) for i in range(0x20, 0x20+(num_chips-8))]
+        for chip in temp:
+            chip.set_i2cBus(2)
+        chips += temp
+
     # init pins
     for i in range(num_pins): # create pins "0", "1", ..., f"{num_pins-1}"
         chips[i//8].pin_mode(f"{i}", "OUTPUT")
@@ -23,18 +65,40 @@ def init_pins() -> list[pcf.PCF]:
     # chips[1] -> "8", "9", "10", "11", "12", "13", "14", "15"
     # chips[2] -> "16", "17", "18", "19", "20", "21", "22", "23"
 
+def init_pi_pins(num_pins, offset) -> None:
+    if num_pins > MAX_PI_PINS:
+        raise Exception("num_pins is too high/not enough pins on the Pi")
+    GPIO.setmode(GPIO.BCM)
+    for i in range(num_pins):
+        GPIO.setup(PI_PIN_MAP[i], GPIO.OUT)
 
-def play_note(note_code):
-    global chips
-    chips[note_code//8].write(f"{note_code}", "HIGH")
+def init_pins(num_pins, offset, mode: str):
+    match mode.upper():
+        case "PI":
+            return init_pi_pins(num_pins, offset)
+        case "I2C":
+            return init_chip_pins(num_pins, offset)
 
-def stop_note(note_code):
-    global chips
-    chips[note_code//8].write(f"{note_code}", "LOW")
+# high_low: "HIGH" or "LOW"
+def note_event_i2c(chips: list[pcf.PCF], note_code, high_low: str):
+    chips[note_code//8].write(f"{note_code}", high_low)
 
-def play_song(song_data):
-    
-    pins = init_pins()
+# high_low: "HIGH" or "LOW"
+def note_event_pi(note_code, on_off: bool):
+    GPIO.output(PI_PIN_MAP[note_code], on_off)
+
+def note_event(offset, num_pins, mode, pins, note_code, on):
+    if note_code >= offset and note_code < offset+num_pins:# in range
+        match mode:
+            case "PI":
+                note_event_pi(note_code, on)
+            case "I2C":
+                note_event_i2c(pins, note_code, "HIGH" if on else "LOW")
+        print(f"pressed note {note_code} {'on' if on else 'off'}")
+
+# mode: "PI" or "I2C"
+def play_song(song_data, mode, num_pins=88, offset=0):
+    pins = init_pins(num_pins, offset, mode)
 
     # loop through each note event
     for note_event in song_data:
@@ -46,14 +110,22 @@ def play_song(song_data):
         sleep(time)
 
         if vel > 0: # note on
-            play_note(note)
+            note_event(offset, num_pins, mode, pins, note, True)
         else: # note off
-            stop_note(note)
+            note_event(offset, num_pins, mode, pins, note, False)
 
-# module initialization
-GPIO.setmode(GPIO.BOARD)
-chips = init_pins()
+    GPIO.cleanup()
 
 # testing
 if __name__ == "__main__":
-    pass
+    import json
+
+    if len(sys.argv) != 5:
+        print("Command Format: python3 control.py json_song_file mode num_pins offset")
+    else:
+        file = open(sys.argv[1], "r")
+        json_data = file.read()
+        file.close()
+        song_data = json.loads(json_data)
+        play_song(song_data, sys.argv[2], int(sys.argv[3]), int(sys.argv[4]))
+    

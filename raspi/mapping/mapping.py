d861a886f22b1beaba0ebf06c4e52a122cc594d9
piano <williampowers@ku.edu>
2024-04-15 13:16:05 -0500

    working, fixed some timing issues

--- raspi/mapping/mapping.py
+++ raspi/mapping/mapping.py
@@ -28,10 +28,20 @@ def Map(file) -> timeline:
         new_note_event: dict = {
             "note": msg_note,
             "velocity": msg_vel,
-            "time": msg_time + (0 if msg_time == 0 else 0.05)
+            "time": msg_time
         }
 
         delta_time = 0
         mapping.append(new_note_event)
+    
+    for i in range(len(mapping)):
+        if i == 0 or i == len(mapping)-1:
+            continue
+        if mapping[i]["note"] == mapping[i-1]["note"] and mapping[i]["note"] == mapping[i+1]["note"]:
+            if mapping[i+1]["time"] < 0.05 and mapping[i+1]["time"] > 0:
+                delta_time = 0.05 - mapping[i+1]["time"]
+                mapping[i+1]["time"] += delta_time
+                mapping[i]["time"] -= delta_time/2
+                mapping[i+2]["time"] -= delta_time/2
 
     return mapping
